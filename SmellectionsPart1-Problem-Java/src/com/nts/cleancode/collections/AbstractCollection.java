f5ad8f682f9daa8213d4eef95acf9efb2b6fa079
shiningyu <shinik77@naver.com>
2016-06-09 22:25:51 +0900

    last update

--- SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/AbstractCollection.java
+++ SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/AbstractCollection.java
@@ -1,8 +1,11 @@
 package com.nts.cleancode.collections;
 
 public abstract class AbstractCollection {
-	protected Object[] elements = new Object[10];
-
+	private static int INITIAL_CAPACITY = 10;
+	protected Object[] elements = new Object[INITIAL_CAPACITY];
+	protected int size = 0;
+    protected boolean readOnly;
+    
 	public void addAll(AbstractCollection collection) {
 		for (int i=0; i < collection.size(); i++) {
 			if (!contains(collection.get(i))) {
@@ -11,13 +14,79 @@ public abstract class AbstractCollection {
 		}
 	}
 
-	public abstract boolean isEmpty();
-	public abstract void add(Object element);
-	public abstract boolean remove(Object element);
-	public abstract boolean contains(Object element);
-	public abstract int size();
+	public boolean isEmpty()
+	{
+    	return size == 0;
+	}
+	protected void addElement(Object element) {
+        elements[size++] = element;
+    }
+	public void add(Object element)
+	{
+		if (readOnly)
+    	    return;
+    	
+    	if (shouldGrow()) {
+    	    grow();
+    	}
+    	
+    	addElement(element);
+	}
+	protected boolean shouldGrow() {
+        return size + 1 > elements.length;
+    }
+	protected void grow() {
+        Object[] newElements =
+                new Object[elements.length + INITIAL_CAPACITY];
+        for (int i = 0; i < size; i++)
+            newElements[i] = elements[i];
+        elements = newElements;
+    }
+	public boolean remove(Object element)
+	{
+		if (readOnly)
+    		return false;
+    	else 	
+    		for (int i = 0; i < size; i++)
+    			if (elements[i].equals(element)) {
+    				removeElementAt(i);
+    				return true;
+    			}
+    	return false;
+	}
+	protected void removeElementAt(int i) {
+        elements[i] = null;
+        Object[] newElements = new Object[size - 1];
+        int k = 0;
+        for (int j = 0; j < size; j++) {
+        	if (elements[j] != null)
+        		newElements[k++] = elements[j];
+        }
+        size--;
+        elements = newElements;
+    }
+	public boolean contains(Object element)
+	{
+		for (int i=0; i<size; i++) 
+    		if (elements[i].equals(element))
+    			return true;
+    	return false;
+	}
+	public Object getElementAt(int index) {
+    	return elements[index];
+    }
+	public int size()
+	{
+		return size;
+	}
 
 	public Object get(int i) {
 		return elements[i];
 	}
+	public int capacity() {
+    	return elements.length;
+    }
+	public void setReadOnly(boolean b) {
+    	readOnly = b;
+    }
 }
--- SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/Map.java
+++ SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/Map.java
@@ -1,35 +1,50 @@
 package com.nts.cleancode.collections;
 
-public class Set extends AbstractCollection {
+public class Map {
 	private static int INITIAL_CAPACITY = 10;
+	protected Object[] keys = new Object[INITIAL_CAPACITY];
+	protected Object[] values = new Object[INITIAL_CAPACITY];
+	private int indexWhereKeyFound;
 	private int size = 0;
 	private boolean readOnly;
 
 	public boolean isEmpty() {
 		return size == 0;
 	}
-
-	public void add(Object element) {
+	public void addAll(Map m){
+		for (int i=0; i<m.size(); i++) 
+			add(m.keys[i], m.values[i]);	
+	}
+	public void add(Object key, Object value) {
 		if (!readOnly) {
+			for (int i = 0; i < size; i++)
+                if (keys[i].equals(key)) {
+                    values[i] = value;
+                    return;
+                }
 			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + INITIAL_CAPACITY];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
+			if (newSize > keys.length) {
+				Object[] newKeys =
+					new Object[keys.length + INITIAL_CAPACITY];
+				Object[] newValues = new Object[keys.length + INITIAL_CAPACITY];
+				System.arraycopy(keys, 0, newKeys, 0, size);
+                System.arraycopy(values, 0, newValues, 0, size);
+                keys = newKeys;
+                values = newValues;
 			}
 
-			if (contains(element))
-				return;
-			elements[size++] = element;
+			keys[size] = key;
+            values[size] = value;
+            size++;
 		}
 	}
 
-	public boolean contains(Object element) {
+	public boolean contains(Object value) {
 		for (int i = 0; i < size; i++)
-			if (elements[i].equals(element))
+			if ((value == null && values[i] == null) || (values[i] != null && values[i].equals(value))){
+				indexWhereKeyFound = i;
 				return true;
+			}
 		return false;
 	}
 
@@ -37,31 +52,35 @@ public class Set extends AbstractCollection {
 		return size;
 	}
 
-	public boolean remove(Object element) {
+	public boolean remove(Object key) {
 		if (readOnly)
 			return false;
 		for (int i = 0; i < size; i++)
-			if (elements[i].equals(element)) {
-				elements[i] = null;
-				Object[] newElements = new Object[size - 1];
-				int k = 0;
-				for (int j = 0; j < size; j++) {
-					if (elements[j] != null)
-						newElements[k++] = elements[j];
-				}
+			if (keys[i].equals(key)) {
+				keys[i] = null;
+				values[i]=null;
 				size--;
-				elements = newElements;
 				return true;
 			}
 		return false;
 	}
+	public boolean containsKey(Object key) {
+        for (int i = 0; i < size; i++)
+            if (keys[i] != null && keys[i].equals(key)) {
+                indexWhereKeyFound = i;
+                return true;
+            }
+        return false;
+    }
+	public Object get(Object key) {
+        if (!containsKey(key))
+            return null;
+        return values[indexWhereKeyFound];
+    }
 
-	public Object getElementAt(int index) {
-		return elements[index];
-	}
 
 	public int capacity() {
-		return elements.length;
+		return keys.length;
 	}
 
 	public void setReadOnly(boolean b) {
