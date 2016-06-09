3b6ad25e86de183239905db74ee0e0201e0711bb
ksdf852 <ksdf852@naver.com>
2016-06-09 17:18:02 +0900

    Modifiy the List.Class

--- SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/List.java
+++ SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/List.java
@@ -10,19 +10,30 @@ public class List extends AbstractCollection {
 	}
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + 10];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
-			elements[size++] = element;
-		}
+		if (readOnly)
+			return;
+		
+		if (shouldGrow())
+			grow();
+		
+		addElement(element);
+	}
+	private boolean shouldGrow() {
+		return (size + 1) > elements.length;
 	}
 
+	private void grow() {
+		Object[] newElements =
+			new Object[elements.length + 10];
+		for (int i = 0; i < size; i++)
+			newElements[i] = elements[i];
+		elements = newElements;
+	}
+	
+	private void addElement(Object element) {
+		elements[size++] = element;
+	}
+	
 	public boolean contains(Object element) {
 		for (int i=0; i<size; i++) 
 			if (elements[i].equals(element))
