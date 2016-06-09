1bba74057fddaec77eb47515f48ad483dab8a626
ParkSungjin <ParkSungjin@ParkSungjinui-MacBook-Pro.local>
2016-06-09 19:28:06 +0900

    part2 AbstractCollection

--- SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/AbstractCollection.java
+++ SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/AbstractCollection.java
@@ -1,16 +1,23 @@
 package com.nts.cleancode.collections;
 
-public abstract class AbstractCollection{
-	public void addAll(AbstractCollection c) {	
-		if (c instanceof Set) {
-			Set s = (Set)c;		
+public abstract class AbstractCollection {
+	protected Object[] elements = new Object[10];
+
+	public void addAll(AbstractCollection collection) {
+		for (int i=0; i < collection.size(); i++) {
+			if (!contains(collection.get(i))) {
+				add(collection.get(i));
+			}
 		}
 	}
+
 	public abstract boolean isEmpty();
+	public abstract void add(Object element);
 	public abstract boolean remove(Object element);
 	public abstract boolean contains(Object element);
-	public abstract int size();	
-	
-	public void add(Object key, Object value) {
+	public abstract int size();
+
+	public Object get(int i) {
+		return elements[i];
 	}
 }
